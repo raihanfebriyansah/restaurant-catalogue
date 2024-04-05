bf26aa19abdb1f41998614ce28757ff1a00c3834
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-06 00:54:56 +0700

    refactor: change the entire code to match the esLint error

--- src/scripts/components/Navbar.js
+++ src/scripts/components/Navbar.js
@@ -4,7 +4,7 @@ class Navbar extends HTMLElement {
       <nav class="navbar" id="sticky">
         <a href="/">
           <figure class="navlogo">
-            <img src="images/logos/logo.png" alt="Burger Santuy" width="50" />
+            <img src="images/logos/logo.png" alt="Daftar Restoran" width="50" />
             <figcaption>
               <h5>DAFRA</h5>
             </figcaption>
@@ -12,16 +12,16 @@ class Navbar extends HTMLElement {
         </a>
         <div>
           <ul class="navbar-item" id="mobile">
-            <li><a class="nav-item active" href="#/">Beranda</a></li>
-            <li><a class="nav-item" href="#/favorit">Favorit</a></li>
-            <li><a class="nav-item" href="https://github.com/reeansa">Tentang Kami</a></li>
+            <li><a class="active" href="#/">Beranda</a></li>
+            <li><a class="" href="#/favorit">Favorit</a></li>
+            <li><a class="" href="https://github.com/reeansa">Tentang Kami</a></li>
           </ul>
           <a id="hamburger" href="#">☰</a>
         </div>
         <ul class="navbar-item-desktop" id="desktop">
-          <li><a class="nav-item active" href="#/">Beranda</a></li>
-          <li><a class="nav-item" href="#/favorit">Favorit</a></li>
-          <li><a class="nav-item" href="https://github.com/reeansa">Tentang Kami</a></li>
+          <li><a class=" active" href="#/">Beranda</a></li>
+          <li><a class="" href="#/favorit">Favorit</a></li>
+          <li><a class="" href="https://github.com/reeansa">Tentang Kami</a></li>
         </ul>
       </nav>`;
 
@@ -30,34 +30,25 @@ class Navbar extends HTMLElement {
       const hamburger = document.querySelector('#hamburger');
       const navItems = document.querySelectorAll('#desktop li a');
       const logoText = document.querySelector('.navbar .navlogo figcaption h5');
-      if (window.innerWidth > 1200) {
-        navItems.forEach((nav) => {
-          const color = nav.classList.contains('active') ? '' : '#421908';
-          nav.style.color = color;
-        });
-      }
       if (window.scrollY > 100) {
         navbar.classList.add('sticky', 'bg-flush-orange-50');
         hamburger.style.color = '#421908';
         logoText.style.color = '#421908';
         navItems.forEach((nav) => {
-          const color = nav.classList.contains('active') ? '' : '#421908';
-          nav.style.color = color;
+          const navItem = nav;
+          navItem.style.color = navItem.classList.contains('active') ? '' : '#421908';
         });
       } else {
         navbar.classList.remove('sticky', 'bg-flush-orange-50');
-        logoText.style.color = '';
         hamburger.style.color = '';
+        logoText.style.color = '';
         navItems.forEach((nav) => {
-          const color = nav.classList.contains('active') ? '' : '';
-          nav.style.color = color;
+          const navItem = nav;
+          navItem.style.color = '';
         });
       }
     };
-
     window.addEventListener('scroll', handleScroll);
-    window.addEventListener('resize', handleScroll);
   }
 }
-
 customElements.define('navbar-component', Navbar);
