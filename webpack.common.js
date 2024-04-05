0efda0166c0a8ff044217e102c263243220fda92
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-06 00:52:35 +0700

    build: removing the "sw" entry adds mode to the module and pathinfo to the output

--- webpack.common.js
+++ webpack.common.js
@@ -2,16 +2,19 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+    // sw: path.resolve(__dirname, 'src/scripts/sw.js'),
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    pathinfo: false,
   },
   module: {
     rules: [
@@ -40,6 +43,25 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/list'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurants-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/images/small/{id}'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurants-image-api',
+          },
         },
       ],
     }),
