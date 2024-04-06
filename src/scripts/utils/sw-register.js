04c29633a72aace836526fa24a304fccde941f5e
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-06 16:24:47 +0700

    refactor: removes esLint error namely disable-next-line no-console

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -2,6 +2,7 @@ import { Workbox } from 'workbox-window';
 
 const swRegister = async () => {
   if (!('serviceWorker' in navigator)) {
+    // eslint-disable-next-line no-console
     console.log('Service Worker not supported in the browser');
     return;
   }
@@ -10,8 +11,10 @@ const swRegister = async () => {
 
   try {
     await wb.register();
+    // eslint-disable-next-line no-console
     console.log('Service worker registered');
   } catch (error) {
+    // eslint-disable-next-line no-console
     console.log('Failed to register service worker', error);
   }
 };
