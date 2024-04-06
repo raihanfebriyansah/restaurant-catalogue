99848c78a4c6f109e6607b430afea39b59caafcc
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-06 16:26:22 +0700

    build: added skip to content function

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -24,6 +24,11 @@ class App {
     const page = routes[url];
     this.contents.innerHTML = await page.render();
     await page.afterRender();
+    const skipLinkElem = document.querySelector('.skip-content');
+    skipLinkElem.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector('#content').focus();
+    });
   }
 }
 
