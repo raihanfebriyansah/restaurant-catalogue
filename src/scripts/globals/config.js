ccfbfaa8daa3b46c03f351406ccce1911c8c3488
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-06 16:23:44 +0700

    refactor: change cache_name to match today's date

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
   BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: 'Dafra-V1',
+  CACHE_NAME: new Date().toISOString(),
   DATABASE_NAME: 'dafra-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
