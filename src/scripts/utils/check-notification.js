8b639c5ef30374dd09e5bdc3e3fb9fe840ea4fdb
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-06 16:24:36 +0700

    refactor: removes esLint error namely disable-next-line no-console

--- src/scripts/utils/check-notification.js
+++ src/scripts/utils/check-notification.js
@@ -5,8 +5,10 @@ const checkNotification = () => {
     swRegister();
     Notification.requestPermission().then((permission) => {
       if (permission === 'granted') {
+        // eslint-disable-next-line no-console
         console.log('Notification permission granted.');
       } else {
+        // eslint-disable-next-line no-console
         console.log('Notification permission denied.');
       }
     });
