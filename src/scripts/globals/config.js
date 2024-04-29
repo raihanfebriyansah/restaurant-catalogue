6d999db8b4868dc420377e05a0f71dc900841362
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-29 13:32:39 +0700

    refactor(config database): perubahan penampungan data dari single quotes menjadi double quotes dan perubahan pada cache name

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-  BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/small/',
-  DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString(),
-  DATABASE_NAME: 'dafra-database',
+  BASE_URL: "https://restaurant-api.dicoding.dev/",
+  BASE_IMAGE_URL: "https://restaurant-api.dicoding.dev/images/small/",
+  DEFAULT_LANGUAGE: "en-us",
+  CACHE_NAME: "DaFra-V1",
+  DATABASE_NAME: "dafra-database",
   DATABASE_VERSION: 1,
-  OBJECT_STORE_NAME: 'restaurants',
+  OBJECT_STORE_NAME: "restaurants",
 };
 
 export default CONFIG;
