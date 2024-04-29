80b6c24db4312ac67f943b60fdc4e2d503f701d4
Raihan Febriyansah <raihanfebriyansahh@gmail.com>
2024-04-29 22:55:23 +0700

    fix(favorite): ubah config image ke url small

--- src/scripts/views/pages/favorite/favorite-list.js
+++ src/scripts/views/pages/favorite/favorite-list.js
@@ -3,7 +3,7 @@ import CONFIG from '../../../globals/config';
 
 const daftarFavorite = (restaurants) => `<article class="card" tabindex="0">
     <figure>
-    <img src="${CONFIG.BASE_IMAGE_URL + restaurants.pictureId}" alt="Gambar ${restaurants.name}">
+    <img src="${CONFIG.BASE_IMAGE_URL_SMALL + restaurants.pictureId}" alt="Gambar ${restaurants.name}">
       <figcaption>
         <h6 class="city">${restaurants.city}</h6>
         <h6 class="rating">${restaurants.rating}%</h6>
